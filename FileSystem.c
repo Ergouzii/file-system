0df15e3e5d3c05f05154023939af15357297c219
Ergouzii <xiaohui1@ualberta.ca>
2019-11-19 21:33:22 -0700

    free-space list check

--- FileSystem.c
+++ FileSystem.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <sys/mount.h>
 #include <unistd.h>
 #include "FileSystem.h"
 
-#define ONE_KB 1024 
+#define ONE_KB 1024
+#define NUM_INODES 126
 
 char buffer[ONE_KB];
 char *cwd;                                 // keep track of current working dir
+Super_block super_block;                                  // init a super block
+
 
 /*
 Mounts the file system for the given virtual disk (file) and sets the current
@@ -17,16 +21,46 @@ Usage: M <disk name>
 */
 void fs_mount(char *new_disk_name) {
 
-    if (access(new_disk_name, F_OK) != 0) {      // if disk name does not exist
+    FILE *disk;
+
+    disk = fopen(new_disk_name, 'r');
+    if (disk == NULL) {      // if disk name does not exist
         fprintf(stderr, "Error: Cannot find disk %s", new_disk_name);
+        exit(1);
     }
 
-    // mounting the file system to disk
-    Super_block super_block;
+    // read the super block from disk
+    fread(&super_block, sizeof(Super_block), ONE_KB, disk);
+    char *free_block_arr = super_block.free_block_list;
+    Inode *inode_arr = super_block.inode;
 
+    //------------------------------------------------------------------------
     // consistency checks:
+    //------------------------------------------------------------------------
     // 1. free-space list check
-    
+    uint8_t inode_in_use = -1;
+    for (int i = 0; i < NUM_INODES; i++) { // 126 is the size of the inode list
+        uint8_t start_block = inode_arr[i].start_block;
+        uint8_t used_size = inode_arr[i].used_size;
+        inode_in_use = used_size >> 7;
+        uint8_t file_size;
+        file_size = used_size << 1;
+        file_size = used_size >> 1;              // inode status bit is removed
+
+        for (int j = 0; j < file_size; j++) {
+            uint8_t start_index = (start_block + j) / 8; // index of block in free block arr
+            uint8_t start_pos = (start_block + j) % 8;
+            uint8_t byte = free_block_arr[start_index];
+            uint8_t bit = byte & (1 << (7 - start_pos));    // mask out the bit
+            bit = bit >> (7 - start_pos);
+            if ((bit ^ inode_in_use) != 0) {
+                fprintf(stderr, "Error: File System in %s is inconsistent (error \
+                    code: %d)", new_disk_name, 1);
+                exit(1);
+            }
+        }
+    //------------------------------------------------------------------------
+
 }
 
 
@@ -37,9 +71,9 @@ Usage: C <file name> <file size>
 */
 void fs_create(char name[5], int size);
 
+
 /*
 Deletes the specified file from current working dir.
-
 
 Usage: D <file name>
 */
